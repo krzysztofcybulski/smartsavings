a0f06e197ef2fd50f546e364718cf5da8d5d1ef7
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-05-10 23:46:49 +0200

    Add simple Rest API

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.0"
+    groovy
 }
 
 group = "me.kcybulski"
@@ -10,5 +11,22 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(project(":coinpaprika"))
     implementation(kotlin("stdlib"))
+
+    implementation("io.ratpack:ratpack-core:1.9.0-rc-2")
+    implementation("io.ratpack:ratpack-reactor:1.9.0-rc-2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.+")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.+")
+    implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
+    implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
+
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.7")
+    testImplementation("org.spockframework:spock-core:2.0-M5-groovy-3.0")
+    testImplementation("io.ratpack:ratpack-groovy-test:1.9.0-rc-2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
