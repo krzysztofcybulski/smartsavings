1f4c8974a93dc58dc0eb27957be2f6ae1cf42491
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-05-18 00:08:09 +0200

    Save to cache

--- redis/src/main/kotlin/me/kcybulski/smartsavings/redis/RedisCacheCryptoPrices.kt
+++ redis/src/main/kotlin/me/kcybulski/smartsavings/redis/RedisCacheCryptoPrices.kt
@@ -23,19 +23,19 @@ class RedisCacheCryptoPrices(
             .doOnNext { println("Loaded from cache ${base.symbol} ${quote.symbol} ${it.first} -> ${it.second}") }
             .cache()
 
-        val fetched = cache.collectList()
+        return cache.collectList()
             .map { cached -> days - cached.map { it.first } }
             .flatMapMany { cryptoPrices.getExchange(base, quote, it) }
-            .doOnNext { persist(base, quote, it.first, it.second) }
-            .doOnNext { println("Savings to cache ${base.symbol} ${quote.symbol} ${it.first} -> ${it.second}") }
-
-        return concat(cache, fetched)
+            .flatMap { persist(base, quote, it.first, it.second) }
+            .doOnNext { println("Saving to cache ${base.symbol} ${quote.symbol} ${it.first} -> ${it.second}") }
+            .concatWith(cache)
     }
 
     private fun persist(base: Coin, quote: Coin, day: LocalDate, value: BigDecimal) =
         redis
             .getBucket<BigDecimal>(key(base, quote, day))
             .set(value)
+            .map { day to value }
 
     private fun key(base: Coin, quote: Coin, day: LocalDate) = "_ex_${base.symbol}_${quote.symbol}_${day}"
 
